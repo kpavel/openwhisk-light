1880c3f8eb596d6e18b1d0146160e057f098d4bb
Pavel Kravchenko <kpavel@il.ibm.com>
2017-05-10 18:08:17 +0300

    refactoring

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var openwhisk = require('openwhisk');
 var bodyParser = require('body-parser');
 var app = express();
 
@@ -12,7 +13,12 @@ var dockerhost = process.env.DOCKER_HOST || function() {
     throw "please set the DOCKER_HOST environmental variable";
 }();
 
+var openwhiskUrl = process.env.OPENWHISK_URL || function() { 
+    throw "please set the OPENWHISK_URL environmental variable pointing to openwhisk global";
+}();
+
 console.log("DOCKERHOST: " + dockerhost);
+console.log("OPENWHISK_URL: " + openwhiskUrl);
 var client = new LocalClient({dockerurl: dockerhost});
 
 const openwhisklocal = require("./openwhisklocal.js") || {}; // holds node specific settings, consider to use another file, e.g. openwhisklocal.js as option
@@ -21,6 +27,7 @@ const burstOWService = process.env.BURST_OW_SERVICE || openwhisklocal.burstOWSer
 
 app.post('/namespaces/:namespace/actions/:actionName', function(req, res) {
   console.log("req: " + JSON.stringify(req.body));
+  console.log("authorization: " + JSON.stringify(req.authorization));
   console.log("PATH: " + req.path);
   console.log("params: " + JSON.stringify(req.params));
   console.log("namespace: " + req.params.namespace);
@@ -32,11 +39,53 @@ app.post('/namespaces/:namespace/actions/:actionName', function(req, res) {
   client.invoke(req.params.actionName, req.body)
     .then((result) => {
        console.log("result: " + JSON.stringify(result));
-       res.send(result);
+       res.send({response: {result}});
     })
     .catch(function(e) {
       console.log("there was invoke error: " + e);
-      if(e == messages.TOTAL_CAPACITY_LIMIT){
+      if(e == messages.ACTION_MISSING_ERROR){
+    	  
+    	console.log("getting action " + req.params.actionName + " from " + openwhiskUrl);
+    	getAction(req.params.namespace, req.params.actionName, req)
+    	.then((action)=>{
+    		console.log("got action: " + JSON.stringify(action));
+        	console.log("Registering action under openwhisk edge " + JSON.stringify(action));
+	    	
+	    	client.create(req.params.actionName, action)
+	        .then((result) => {
+	           console.log("action " + req.params.actionName + " registered: " + JSON.stringify(result));
+	           client.invoke(req.params.actionName, req.body)
+	           .then((result) => {
+	              console.log("result of invoke: " + JSON.stringify(result));
+	              res.send({response: {result}});
+	           })
+	           .catch(function(e) {
+	             console.log("there was invoke error : " + e);
+	           });
+	        })
+	        .catch(function(e) {
+	          console.log(e);
+	          res.status(500).send(e);
+	          return;
+	        })
+
+    	      if(burstOWService){
+    	        client.request("PUT", burstOWService + req.path, req.body).then(function(result){
+    	          console.log("--- RESULT: " + JSON.stringify(result));
+    	          res.send(result);
+    	        }).catch(function(e) {
+    	          console.log("--- ERROR registering action in bursting!: " + JSON.stringify(e));
+    	        });
+    	      }
+
+    	})
+    	.catch(function (err) { 
+			console.log("action get error: " + err);
+			res.status(404).send(err);
+			return;
+	    });
+    	  
+      } else if(e == messages.TOTAL_CAPACITY_LIMIT){
         console.log("Maximal local capacity reached, delegating action invoke to bursting ow service");
 
         if(burstOWService){
@@ -64,7 +113,8 @@ app.put('/namespaces/:namespace/actions/:actionName', function(req, res) {
   console.log("actionName: " + req.params.actionName);
   console.log("BODY: " + JSON.stringify(req.body));
 
-
+  
+  
   client.create(req.params.actionName, req.body)
     .then((result) => {
        console.log("result: " + JSON.stringify(result));
@@ -88,6 +138,62 @@ app.put('/namespaces/:namespace/actions/:actionName', function(req, res) {
     });
   }
 });
+
+
+
+
+
+
+
+
+app.get('/namespaces/:namespace/actions/:actionName', function(req, res) {
+	getAction(req.params.namespace, req.params.actionName, req)
+	.then((action)=>{
+		console.log("get action result: " + JSON.stringify(action));
+		res.send(action);
+	});
+});
+
+app.get('/namespaces', function(req, res) {
+	var api_key = from_auth_header(req);
+	var ow_client = openwhisk({api: openwhiskUrl, api_key});
+	
+	ow_client.namespaces.list()
+    .then((namespaces) => { 
+	    	console.log("get namespaces result: " + JSON.stringify(namespaces));
+	    	res.json(namespaces) 
+    	})
+    .catch(function (err) { console.log("get namespaces error: " + err); res.json({parameters: []});});
+});
+
+function getAction(namespace, actionName, req){
+	var api_key = from_auth_header(req);
+	var ow_client = openwhisk({api: openwhiskUrl, api_key});
+	
+    return new Promise(function(resolve,reject) {
+    	ow_client.actions.get({actionName, namespace})
+	    .then(function (action) {
+	    	console.log("Got action from " + openwhiskUrl + ": " + JSON.stringify(action));
+	    	resolve(action);
+	    })
+	    .catch((e) => {
+	    	reject(e);
+	    });
+    });
+}
+
+function from_auth_header(req) {
+  var auth = req.get("authorization");
+  auth = auth.replace("Basic ", "");
+  auth = new Buffer(auth, 'base64').toString('ascii');
+  return auth;
+}
+
+//createAction(req.params.actionName, action).then((result) => {
+//	  
+//}).catch((e) => {
+//	  
+//});
 
 app.listen(port);
 module.exports = app;

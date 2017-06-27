f0e1b1e48d8d680ca6522888fbd80cbd0ce4b6ab
glikson <glikson@il.ibm.com>
2017-06-27 16:01:14 +0300

    minor updates to config and env variables

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,7 +1,7 @@
 export DIR=$BATS_TEST_DIRNAME
 export BASE_DIR=$DIR/../
 export DOCKER_HOST=tcp://0.0.0.0:2375
-export OW_LOCAL_DOCKER_NW_NAME=owl
-export TOTAL_CAPACITY=5
-export DELEGATE_ON_FAILURE=true
-export RETRIES=0
+export OWL_NET_NAME=owl
+export OWL_TOTAL_CAPACITY=5
+export OWL_DELEGATE_ON_FAILURE=true
+export OWL_RETRIES=0
